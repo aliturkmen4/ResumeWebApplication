76c1c51fe4ecc69962f8b6814c539948dda710fd
Ali <alym11731996@gmail.com>
2022-02-01 14:27:13 +0300

    bootstrap icons was added

---
+++ ResumeWebApplication/Aboutme.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin.Master" AutoEventWireup="true" CodeBehind="Aboutme.aspx.cs" Inherits="ResumeWebApplication.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+değişen alan
+</asp:Content>
--- ResumeWebApplication/Admin.Master.designer.cs
+++ ResumeWebApplication/Admin.Master.designer.cs
@@ -13,5 +13,23 @@ namespace ResumeWebApplication
 
     public partial class Admin
     {
+
+        /// <summary>
+        /// ContentPlaceHolder1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+
+        /// <summary>
+        /// ContentPlaceHolder2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder2;
     }
 }
